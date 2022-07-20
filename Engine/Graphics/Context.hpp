1e957a86c4d9dafcc49547ce63de5800b196d407
Saullo Bretas Silva <saullo.silva55@gmail.com>
2022-07-20 14:23:21 -0300

    Engine: Define OpenGL version (4.6) on Window::on_init()

--- Engine/Graphics/Context.hpp
+++ Engine/Graphics/Context.hpp
@@ -7,6 +7,10 @@ namespace Engine::Graphics
     class Context
     {
     public:
+        static constexpr auto glsl_version = "#version 460";
+        static constexpr auto major_version = 4;
+        static constexpr auto minor_version = 6;
+
         void on_init();
         void on_render();
     };
--- Engine/Window.cpp
+++ Engine/Window.cpp
@@ -1,3 +1,4 @@
+#include <Engine/Graphics/Context.hpp>
 #include <Engine/Window.hpp>
 #include <stdexcept>
 
@@ -11,6 +12,11 @@ namespace Engine
     {
         if (!glfwInit())
             throw std::runtime_error("Unable to init GLFW");
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, Graphics::Context::major_version);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, Graphics::Context::minor_version);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
         m_window = glfwCreateWindow(1024, 768, "Mysteries Of Darkness", nullptr, nullptr);
         if (!m_window)
